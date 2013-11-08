c374bb12ed8e09f0f4af9745bc79acd932cedc33
Wenhui Zhao <wenhuizhao@yahoo.com>
2013-11-07 17:11:45 -0800

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kecheng::Application.config.secret_token = '06359776fbad84fbe3a16add03cf579ffb304531b2d0d5bc036cbc45240bda86884681283be1021c2a6b94e565850c4b032206055e7dad9a420027be99fc039f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kecheng::Application.config.session_store :cookie_store, key: '_kecheng_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kecheng::Application.config.session_store :active_record_store
