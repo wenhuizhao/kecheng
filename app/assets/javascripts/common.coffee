2e7abdacf9de597009f64fecb0f2fe68cf1668bf
sunzhimeng <532681765@qq.com>
2013-11-13 21:09:47 +0800

    make page show better, add beautiful css

---
+++ app/assets/javascripts/common.coffee
@@ -0,0 +1,3 @@
+$(document).ready ->
+  $('.course-toggle-title').click ->
+    $('.courses').toggle()
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
+  
   def index
+    @courses = Course.all
   end
+
 end
