c513a66c0608638c95a1c0352cf91032be6039f4
Wenhui Zhao <wenhuizhao@yahoo.com>
2013-11-15 19:12:11 -0800

    add capistrano

---
+++ spec/factories/exercises.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :exercise do
+    title "MyString"
+    note "MyString"
+    answer "MyString"
+  end
+end
