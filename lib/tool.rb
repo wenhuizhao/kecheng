8b5b2a529592eb7cb92189dada83cb8536143278
sunzhimeng <532681765@qq.com>
2013-11-27 16:36:27 +0800

    add interface for sms

---
+++ lib/tool.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+module Tool
+  def send_sms(opts)
+    default_opts = {
+      action: 'send',
+      account: '',
+      password: ''
+    }
+    url = 'http://www.qf106.com/sms.aspx'
+    post_data(url, default_opts.merge(opts))
+  end
+
+  def post_data(url, pas)
+    uri = URI.parse(URI.encode(url))
+    http = Net::HTTP.new(uri.host, uri.port) 
+    http.use_ssl = true if uri.scheme == 'https'
+    req = Net::HTTP::Post.new(uri.path) 
+    req.set_form_data(pas)
+    res = http.request(req) 
+    res.body  
+  end
+end
