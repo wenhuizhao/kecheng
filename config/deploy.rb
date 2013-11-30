d6f9a9b72a5e9472eb573692627d58c96f7c7e59
Wenhui Zhao <wenhuizhao@yahoo.com>
2013-11-29 21:26:25 -0800

    add deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,16 +22,30 @@ role :db,  "dev.bigsai.com", :primary => true # This is where Rails migrations w
 role :db,  "dev.bigsai.com"
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:symlink_db","deploy:db_migrate", "deploy:db_seed", "deploy:asset", "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-#namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-#end
+namespace :deploy do
+   desc "Symlinks the database.ym"
+   task :symlink_db, :roles => :app do
+     run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+   end
+   desc "Precompile Assets"
+   task :asset, :roles => :app do
+     %x[bundle exec rake assets:precompile]
+   end
+   task :db_migrate, :roles => :db do
+     %x[bundle exec rake db:migrate]
+   end
+   task :db_seed: :roles => :db do
+     %x[bundle exec rake db:seed]
+   end
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
