e7629e6b60be02fefb96793a1b5a793d49c1049a
sunzhimeng <532681765@qq.com>
2013-12-19 16:23:49 +0800

    add school to garde manage pages, fix bugs

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,12 +1,12 @@
 class Grade < ActiveRecord::Base
-  attr_accessible :class_num, :full_name, :grade_num, :period
+  attr_accessible :class_num, :full_name, :grade_num, :period, :school_id
 
   after_create :set_full_name
 
   has_many :grades_courses
   has_many :teachers, through: :grades_courses
-  validates :grade_num, :class_num, presence: true
-
+  validates :grade_num, :class_num,:school_id, presence: true
+  belongs_to :school
   # scope :history_of, -> (user) {all}
   
   def uteachers
@@ -16,6 +16,10 @@ class Grade < ActiveRecord::Base
   def set_full_name
     self.full_name = Name.cname(grade_num, class_num)
     self.save
+  end
+  
+  def school_name
+    school.try :name
   end
 
   module Name
