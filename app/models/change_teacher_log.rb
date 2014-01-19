e7a70e2b3dd5242c825f43f203f4c6b70853ef31
sunzhimeng <532681765@qq.com>
2014-01-19 10:59:12 +0800

    fix

--- app/models/change_teacher_log.rb
+++ app/models/change_teacher_log.rb
@@ -1,3 +1,3 @@
 class ChangeTeacherLog < ActiveRecord::Base
-  attr_accessible :admin_id, :curr_teacher_id, :grades_course_id, :prev_teacher_id
+  attr_accessible :admin_id, :curr_teacher_id, :grades_course_id, :prev_teacher_id, :new_grades_course_id, :message_id
 end
