b4e0b21427d565eabc457ff805c6b3cd9a649fff
Wenhui Zhao <wenhuizhao@yahoo.com>
2014-01-19 18:57:44 -0800

    remove restart task

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,6 @@ set :branch, "develop"
 set :ssh_options, {:forward_agent => true}
 default_run_options[:pty] = true
 
-#set :deploy_to, '/var/www/kecheng'
 set :scm, :git
 set :use_sudo, false
 
@@ -17,16 +16,8 @@ set :user, "deploy"
 
 set :deploy_via, :remote_cache
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-#role :web, "dev.bigsai.com"                          # Your HTTP server, Apache/etc
-#role :app, "dev.bigsai.com"                          # This may be the same as your `Web` server
-#role :db,  "dev.bigsai.com", :primary => true # This is where Rails migrations will run
-#role :db,  "dev.bigsai.com"
-
 # if you want to clean up old releases on each deploy uncomment this:
-after "deploy:create_symlink","deploy:db_migrate", "deploy:db_seed", "deploy:asset", "deploy:restart", "deploy:cleanup"
+after "deploy:create_symlink","deploy:db_migrate", "deploy:db_seed", "deploy:asset", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
