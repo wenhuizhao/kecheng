22cb180c704814ec3ad9c8b292f2b84d1654ce33
sunzhimeng <532681765@qq.com>
2014-01-20 16:32:03 +0800

    add ABCD for the options which did not have them

--- app/models/exercise_option.rb
+++ app/models/exercise_option.rb
@@ -3,4 +3,14 @@ class ExerciseOption < ActiveRecord::Base
   attr_accessible :exercise_id, :name
   belongs_to :exercise
 
+  def full_name
+    index = 0
+    exercise.options.each_with_index do |o, i| 
+      if exercise.options[i] == self
+        index = i
+        break
+      end
+    end
+    name =~ /[A-F]+/ ? name : "#{OPTION_NUM[index]}#{name =~ /^[,\.。、，]+/ ? "" : ". "}#{name}"
+  end
 end
