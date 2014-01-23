ccdf8af37e7a84395dea0ddc94b7adec4cc50f4a
sunzhimeng <532681765@qq.com>
2014-01-23 16:08:43 +0800

    change the message content of approving teachers courses

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -53,13 +53,14 @@ class MessagesController < ApplicationController
   
   def accept_select_grades
     base_p = {sender_id: current_user.id, receiver_id: @message.sender_id, school_id: current_user.school_id}
+    desc_tail = (@message.desc =~ /主持/ ? "#{@message.desc.match(/老师(.*?)[\,，]+/).to_a[1]}" : @message.desc).to_s + '的请求' 
     if params[:status]
       @message.sync_role(:refused)
-      Message.create(base_p.merge(desc: '管理员拒绝了您' + @message.desc + '的请求'))
+      Message.create(base_p.merge(desc: '管理员拒绝了您' + desc_tail))
       redirect_with_message '已经拒绝', action: :index
     else
       @message.sync_role(:approved)
-      Message.create(base_p.merge(desc: '管理员通过了您' + @message.desc + '的请求'))
+      Message.create(base_p.merge(desc: '管理员通过了您' + desc_tail))
       redirect_with_message '已经批准', action: :index
     end
   end
