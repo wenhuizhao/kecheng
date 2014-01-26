7d0b085cf0ad6d0579ae7864d5147d1f848cb61d
sunzhimeng <532681765@qq.com>
2014-01-26 20:29:23 +0800

    fix 123

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,10 @@ class HomeController < ApplicationController
   end
 
   def update_books
-  	books = Book.for_course.select{|b| b.name =~ /#{App::ChineseNum[params[:grade_num].to_i]}/ && b.name =~ /#{Course.find(params[:course_id]).name}/}
+    grade_name = App::ChineseNum[params[:grade_num].to_i]
+    course_name = Course.find(params[:course_id]).name
+  	books = Book.for_course.select{|b| b.name =~ /#{grade_name}/ && b.name =~ /#{course_name}/}
+    @gname = "#{grade_name}年级#{course_name}"
     render partial: 'shared/update_books', locals: {books: books}
   end
 end
