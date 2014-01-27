108b4e3e7979bbaf7b40e96dd0983937b1ab050c
sunzhimeng <532681765@qq.com>
2014-01-27 10:39:18 +0800

    diff wrong and right exercises

--- app/models/student_homeworks_exercises.rb
+++ app/models/student_homeworks_exercises.rb
@@ -1,4 +1,8 @@
 class StudentHomeworksExercises < ActiveRecord::Base
   attr_accessible :canvas, :exercise_id, :student_homework_id, :answer, :teacher_id, :check_desc
   has_many :questions, foreign_key: "student_homeworks_exercise_id"
+
+  def right
+  	check_desc =~ /right/
+  end
 end
