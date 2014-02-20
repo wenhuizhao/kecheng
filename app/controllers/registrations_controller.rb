78ff90c9ed706e1544f89834e9f23feb1c756b1b
sunzhimeng <532681765@qq.com>
2014-02-20 15:17:32 +0800

    skip filter for register

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 class RegistrationsController < Devise::RegistrationsController
   
+  skip_before_filter :require_admin
+  
   def create
     return render_alert '非法註冊' if params[:user][:role_id].nil?
     return render_alert '验证码不正确' if session[:auth_code] != params[:auth_code]
