bb41e9dc75ae6a185f583f2271d866cc4b732709
sunzhimeng <532681765@qq.com>
2014-03-26 21:54:23 +0800

    set default show area

--- app/assets/javascripts/tab.js
+++ app/assets/javascripts/tab.js
@@ -1,8 +1,4 @@
-jQuery.jqtab = function(tabtit,tab_conbox,shijian) {
-  $(tab_conbox).find("li").hide();
-  $(tabtit).find("li:first").addClass("thistab").show(); 
-  $(tab_conbox).find("li:first").show();
-  
+jQuery.jqtab = function(tabtit,tab_conbox,shijian) {  
   $(tabtit).find("li").bind(shijian,function(){
     $(this).addClass("thistab").siblings("li").removeClass("thistab"); 
     var activeindex = $(tabtit).find("li").index(this);
