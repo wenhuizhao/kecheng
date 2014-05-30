cc6ba8386f9f5d42ad43c16749928d5318a79db1
Wayne Zhao <wezhao@cisco.com>
2014-05-30 09:04:25 -0700

    auto check multichoice exercise anser

--- app/controllers/student_homeworks_controller.rb
+++ app/controllers/student_homeworks_controller.rb
@@ -83,6 +83,8 @@ class StudentHomeworksController < ApplicationController
         she.update_attribute :answer, params["#{e.id}option"] if params["#{e.id}option"]
         if e.is_multi_choice? && params["#{e.id}option"].chomp.downcase == e.answer.chomp.downcase
            she.update_attribute :check_desc, 'right'
+        else
+           she.update_attribute :check_desc, 'wrong'
         end
       elsif current_user.is_teacher?
         she.update_attributes(teacher_id: current_user.id, check_desc: params[:check_desc][i])
