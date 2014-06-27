0f039a6f8c4efa2c1c974f9118fb6c81e0bea48a
Wenhui Zhao <wenhuizhao@yahoo.com>
2014-06-26 23:32:01 -0700

    fix bug

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,14 +1,8 @@
 # -*- encoding : utf-8 -*-
 class Category < ActiveRecord::Base
   attr_accessible :name
-  DISPLAY_ORDER=['目标聚焦','新课导入','初读点拨','实践应用','精要交流','达标检测','矫错锦囊','指引问答','拓展延伸','其他']
+  DISPLAY_ORDER=['目标聚焦','新课导入','精要交流','实践应用','初读点拨','达标检测','矫错锦囊','指引问答','拓展延伸','其他']
   def self.display_order ids
     DISPLAY_ORDER.map{|n| Category.find_by_name(n).id if ids.include?(Category.find_by_name(n).id)}.reject{|i| i.nil?}
-  end
-  def self.JingYaoJiaoLiu
-    Category.find_by_name('精要交流')
-  end
-  def self.ChuDuDianBo
-    Category.find_by_name('初读点拨')
   end
 end
