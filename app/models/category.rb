072667301b303af9b0b27ac5d65c5b13f1cb1ed6
Wenhui Zhao <wenhuizhao@yahoo.com>
2014-06-30 19:22:53 -0700

    use display order for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Category < ActiveRecord::Base
-  attr_accessible :name
-  DISPLAY_ORDER=['目标聚焦','新课导入','精要交流','实践应用','初读点拨','达标检测','矫错锦囊','指引问答','拓展延伸','其他']
+  attr_accessible :name, :display_order
   def self.display_order ids
-    DISPLAY_ORDER.map{|n| Category.find_by_name(n).id if ids.include?(Category.find_by_name(n).id)}.reject{|i| i.nil?}
+    Category.find(ids).sort_by{|c| c.display_order}.map(&:id)
   end
 end
